c1b4b8a32f301145e89f7bc6a76fd21562d94dab
samshy87 <120481195+samshy87@users.noreply.github.com>
2022-12-13 08:38:52 -0600

    Update find_missing_location.py
    changed variable names to better reflect the data

--- find_missing_location.py
+++ find_missing_location.py
@@ -1,18 +1,17 @@
 import pandas as pd
-df=pd.DataFrame({'Name':['2022-12-14','2022-12-14','2022-12-14','2022-12-14','2022-11-13','2022-11-13','2022-11-13','2022-11-13','2022-10-12','2022-10-12','2022-10-12','2022-09-11','2022-09-11','2022-09-11','2022-09-11','2022-08-10','2022-08-10','2022-08-10','2022-08-10','2022-07-09','2022-07-09','2022-07-09','2022-07-09'],
-     'FT_Team':['WHITEMAN','BUCKLEY','MAXWELL','EDWARDS','WHITEMAN','BUCKLEY','MAXWELL','EDWARDS','WHITEMAN','BUCKLEY','MAXWELL','WHITEMAN','BUCKLEY','MAXWELL','EDWARDS','WHITEMAN','BUCKLEY','MAXWELL','EDWARDS','WHITEMAN','BUCKLEY','MAXWELL','EDWARDS',]})
-print(df)
-print(" ")
-teamNames = ['WHITEMAN','BUCKLEY','MAXWELL','EDWARDS']
-fNames = (df['Name'].unique())
+df=pd.DataFrame({'DateTime':['2022-12-14','2022-12-14','2022-12-14','2022-12-14','2022-11-13','2022-11-13','2022-11-13','2022-11-13','2022-10-12','2022-10-12','2022-10-12','2022-09-11','2022-09-11','2022-09-11','2022-09-11','2022-08-10','2022-08-10','2022-08-10','2022-08-10','2022-07-09','2022-07-09','2022-07-09','2022-07-09'],
+     'Location':['WHITEMAN','BUCKLEY','MAXWELL','EDWARDS','WHITEMAN','BUCKLEY','MAXWELL','EDWARDS','WHITEMAN','BUCKLEY','MAXWELL','WHITEMAN','BUCKLEY','MAXWELL','EDWARDS','WHITEMAN','BUCKLEY','MAXWELL','EDWARDS','WHITEMAN','BUCKLEY','MAXWELL','EDWARDS',]})
 
-#nested for loop to search through each unique name for a specific team name
-for i in teamNames:
-    for x in fNames:
-        # the ~ in front of df.loc is used to say when x is NOT in FT_Team.  
-        # its used to make 'isin' effectively 'notin'.
-        df2=~df.loc[(df['Name']==x),'FT_Team'].isin([i])
-        print(df2)
+#Locations to search for
+Bases = ['WHITEMAN','BUCKLEY','MAXWELL','EDWARDS']
+#list of unique dates
+Dates = (df['DateTime'].unique())
 
-#each printed line is a boolean check if that Team name is there or not.  
-#To go from here, in your code you'll need to check for a return value of true, that will mean whatever name you are searching for is not there and then do whatever action is required after.
+#nested for loop to search through each unique date for a specific location
+for i in Dates:
+    for x in Bases:
+        df2=df.query("DateTime == @i")
+        df3=df2.query("Location == @x")
+        if df3.empty: print('Location ' + x + ' not found on ' + i)
+
+#Last line checks if the query came back with nothing, and if so prints out that location and time as missing.  In this test dataset, I made the 2022-10-12 date missing just Edwards AFB.
